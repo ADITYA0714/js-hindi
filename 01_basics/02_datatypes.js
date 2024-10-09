0fae6269a5201bc348da08243984064fd7812308
Adi7ya <95866052+ADITYA0714@users.noreply.github.com>
2024-10-09 08:14:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict";//treats all JS as newer version
+
+//alert(3+3)//we are using Nodejs not browser
+
+// number
+// bigint 
+// string => ""
+// boolean => true/false 
+// null
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Aditya")
