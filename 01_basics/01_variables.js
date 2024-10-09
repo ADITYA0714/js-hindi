2e1a799780f9b58c37414f1a587f8eb602f73212
Adi7ya <95866052+ADITYA0714@users.noreply.github.com>
2024-10-09 08:14:46 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 13247;
+let emailId = "aditya@gmail.com";
+var accountPassword  = "62324";
+accountCity = "Ghaziabad";
+
+//accountId = 253465  //not allowed
+emailId = "vikash@google.com"
+accountPassword="356e6";
+accountCity = "Patna"
+
+/* Prefer not to use var 
+as it does not have control over the scope*/
+
+console.log(accountId)
+console.table([accountId,emailId,accountPassword,accountCity]);
