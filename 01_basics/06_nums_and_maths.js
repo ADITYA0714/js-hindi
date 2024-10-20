e18d36b5453cf4a71b6d189450de88fde2f1281c
Adi7ya <95866052+ADITYA0714@users.noreply.github.com>
2024-10-20 05:55:33 +0000

    number and math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,23 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(400)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));//returns value upto decimal place specified --> 400.00
+
+
+const otherNumber = 123.8967
+// console.log(otherNumber.toPrecision(3));//round of after 3 digit
+
+
+
+//++++++++++++++++++++++++++++ Math +++++++++++++++++++++++++++++
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*10)+1)
+
+const max = 20
+const min = 10
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
