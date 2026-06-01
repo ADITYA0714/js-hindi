47ad372fa76e4786a328b59a42ec2d804dd06f31
Adi7ya <95866052+ADITYA0714@users.noreply.github.com>
2026-06-01 06:00:21 +0000

    Update

--- 08_events/changeColor/chaiaurcode.js
+++ 08_events/changeColor/chaiaurcode.js
@@ -4,6 +4,14 @@ const bodyy = document.querySelector('body')
 
 const getRandomColor = () => {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
+
+    // Alternate way to generate Hex code for color:-
+    //const hex = '0123456789ABCDEDF';
+    //let color = '#';
+    // for(let i = 0; i<6 ; i++){
+    //     color += hex[Math.floor(Math.random()*16)];
+    // }
+    // return color;
 };
 
 let colorInterval;
